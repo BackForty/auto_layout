2346b00414fe11011188d9fc31cece4a499bc5cc
Flip Sasser <flip@inthebackforty.com>
2013-03-07 11:16:44 -0500

    Initial Commit

---
+++ Whatever/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Whatever
+//
+//  Created by Flip Sasser on 3/7/13.
+//  Copyright (c) 2013 Logline. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Whatever/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Whatever
+//
+//  Created by Flip Sasser on 3/7/13.
+//  Copyright (c) 2013 Logline. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
